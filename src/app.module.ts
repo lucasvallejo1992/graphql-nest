55baef335e9bba5f744493320c1efe1adbaf545e
Lucas Vallejo <lucasvallejo92@gmail.com>
2022-12-20 02:06:07 -0300

    ✨ feat: set apollo studio

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ExampleModule } from './example/example.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
-      // playground: false
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     ExampleModule,
