a2437852ffcc1a551c4bc9266345f8ef62a42be2
Lucas Vallejo <lucasvallejo92@gmail.com>
2022-12-21 20:35:07 -0300

    ✨ feat: add query with arguments

--- src/example/example.resolver.ts
+++ src/example/example.resolver.ts
@@ -1,9 +1,31 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class ExampleResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    name: 'example',
+    description: 'This returns a plain test text',
+  })
   example(): string {
     return 'This is a test';
+  }
+
+  @Query(() => Float, {
+    name: 'getRandomNumber',
+    description: 'Generates a random number',
+  })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'getRandomNumberFromZeroTo',
+    description: 'Generates a random number from 0 to N',
+  })
+  getRandomNumberFromZeroTo(
+    @Args('to', { type: () => Int })
+    to: number,
+  ): number {
+    return Math.floor(Math.random() * to);
   }
 }
